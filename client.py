9ece2de68b1137f3a03af0f3c21bb4734c5577c8
Znerual <laurenz_ruzicka@gmx.at>
2017-03-20 22:32:44 +0100

    add comments

--- client.py
+++ client.py
@@ -1,6 +1,11 @@
 from configuration import Configuration
+
+#test the configuration class:
+#login credentials:
 conf = Configuration()
 us, pw = conf.getUsernamePassword('Laurenz')
+
+#course entries:
 print(us + ":" + pw)
 id, date, place = conf.getCourseByName('GDPII')
 print(str(id) + ", " + str(date) + " - " + str(place))
